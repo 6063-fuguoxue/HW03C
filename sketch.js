60e9491d6bc1609ec65ea9f68c2db33c3105f984
Fuguo Xue <fx2100@nyu.edu>
2023-09-23 22:17:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,29 @@
+let numDots = 200; 
+let numLines = numDots / 2;
+let dot = {}; // store a random point position
+let dots = []; // store all the dot positions
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  randomSeed(90);
+  strokeWeight(1);
+
+  // Randomly generate dot positions
+  for (let i=0; i<numDots; i++){
+    dot = {
+      x: random(windowWidth),
+      y: random(windowHeight)
+      // connected: false
+    }
+    dots.push(dot);
+  }
+
+  // Solution 1: connect dots in pairs
+  for (let j=0; j<numLines; j++){
+    line(dots[j].x, dots[j].y, dots[j+numLines].x, dots[j+numLines].y);
+  }
+  
 }
